18c5f0067c1e8a1582441854fcb0f59d05bcf3ec
abalam0 <abalamhossain@gmail.com>
2025-02-18 23:39:35 +0600

    class08

--- class07/class07.js
+++ class07/class07.js
@@ -1,31 +1,31 @@
 
 
-// This class about function.
+/* This class about function. */
 
 
-isCartEmpty();
+// isCartEmpty();
 
-var shoppingCart='';
-
-
-function isCartEmpty (cart /* peramitar*/){
-    if(cart){
-        console.log('The cart is not empty');
-    }else{
-        console.log('The cart is empty');
-    }
-
-    console.log(cart)
-}
-
-shoppingCart='keya bat hey';
-isCartEmpty(shoppingCart  /* argument*/);
-
-shoppingCart='hai';
-isCartEmpty(shoppingCart);
+// var shoppingCart='';
 
 
-// function about a number is even or odd.
+// function isCartEmpty (cart /* peramitar*/){
+//     if(cart){
+//         console.log('The cart is not empty');
+//     }else{
+//         console.log('The cart is empty');
+//     }
+
+//     console.log(cart)
+// }
+
+// shoppingCart='keya bat hey';
+// isCartEmpty(shoppingCart  /* argument*/);
+
+// shoppingCart='';
+// isCartEmpty(shoppingCart);
+
+
+/*function about a number is even or odd */
 
 function evenOrodd (num){
     if (num % 2 === 0){
@@ -35,6 +35,32 @@ function evenOrodd (num){
     }
 }
 
-evenOrodd(222992504);
+evenOrodd(2229925043);
 
-// Single parameter , multiple arguments
+/* multiple parameter , multiple arguments*/
+
+function addition (num1 , num2){
+    return num1 + num2;
+}
+
+console.log(addition(45,40));
+
+
+
+/* three parameter , two  arguments. (আর্গুমেন্ট থাইকা প্যারামিটার একটা বেশি থাকলে কি হবে?)*/
+
+function addition2 (num1 , num2,num3){
+    return num1 + num2 + num3;
+}
+console.log(addition2(45,40)); /* ফলাফল NAN আসবে। কারণ প্রথম দুইটা নাম্বার এর মান আছে কিন্তু তিন নাম্বার এর মান নাই যা undefind  দিবে আর দুইটা নাম্বার আর একটা undefind মিলে পুরোটা নাম্বার নয় বুজাবে মানে NAN দেখাবে */ 
+
+
+/* three parameter , two  arguments.( নান যাতে না আসে টার জন্য করনীয়)*/
+
+function addition2 (num1 , num2,num3 = 0){
+    return num1 + num2 + num3;
+}
+console.log(addition2(45,40,80)) ;/* এখানে যখন  num3 আর্গুমেন্ট থাকবে না অর্থাৎ num3 এর মান থাকবে না তখন সে তার মান (০) নিবে আর আর্গুমেন্ট থাকলে ঐ আর্গুমেন্ট এর মানটাই নিবে।   */
+
+
+
--- class08/class08.js
+++ class08/class08.js
@@ -1,66 +1,62 @@
 
 
 
-// function day (time){
-//     if (time>=6 && time<12){
-//         console.log('Morning');
-//     }else if (time>=12 && time<=18){
-//         console.log('Afternoon');
-//     }else if (time>1 && time<6){
-//         console.log('Night');
-//     }else{
-//         console.log('Invalide Time');
-//     }
-// }
 
-// day(11);
+/* This class about Array.*/
 
+var shoppingCart = ['Alam','kayes', 'Jorina','Morjina'];
 
-
-
-
-// This class about Array.
-
-var shoppingCart = ['Alam ','kayes', 'Jorina ','Morjina'];
-console.log(`There are ${shoppingCart.length} ittems in the shopping cart`);
+// console.log(`There are ${shoppingCart.length} ittems in the shopping cart`); /* এখানে ভেরিয়েবল এর নামের পাশে length প্রপার্টি টা  আরের মধ্যে কইটা আইটেম আছে টা জানতে ব্যাবহার করা হয়  */
 
 // shoppingCart.push('Shavana');
 // shoppingCart.push('kulsum');
-// shoppingCart.push('kabila');
 
 // console.log(`There are ${shoppingCart.length} ittems in the shopping cart`);
-// console.log(`Recently added: ${shoppingCart[shoppingCart.length - 1]}`);
+
+// shoppingCart.push(null);/*এখানে  null টা এরকম ভেলু  */
+// console.log(5 + null); /*এখানেও  null টা এরকম ভেলু  */
+
+// console.log(shoppingCart[0]); /* অ্যারের এর পাশে [0] টা দিয়া ভিতরের প্রথম  আইটেম  গুলারে কল করা হয়। আইটেম গুলার ইনডেক্স নাম্বার শুরু হয় 0 দিয়া। অর্থাৎ প্রথম আইটেম এর ইনডেক্স নাম্বার 0 তার পরে 1,2,3,4,5.......   */
+ 
+/*  সর্বশেষ  অ্যাড করা আইটেম টা যেভাবে দেখব*/
+
+// console.log(`Recently added: ${shoppingCart[shoppingCart.length - 1]}`); /* length মোট আইটেম সংখ্যা আউট পুট দেই। আর মোট আইটেম সংখ্যা থেকে সর্বশেষ আইটেম এর ইনডেক্স  নাম্বার 1 কম থাকে।তাই সর্বশেষ length থাইকা 1 মাইনাস করা হয়েছে    */
+
+/*  মোট আইটেম আর সর্বশেষ অ্যাড করা আইটেম দেখার জন্য  ফাংশন   */
+
+// function cartDisplay(cart) {
+//     console.log(`There are ${shoppingCart.length} ittems in the shopping cart`);
+//     console.log(`Recently added: ${shoppingCart[shoppingCart.length - 1]}`); 
+// }
+
+// shoppingCart.push('Shavana');
+// cartDisplay(shoppingCart);
+// shoppingCart.push('kulsum');
+// cartDisplay(shoppingCart);
+
+/* return দিয়ে মোট আইটেম আর সর্বশেষ অ্যাড করা আইটেম দেখার জন্য  ফাংশন   */
+
+// function cartDisplay(cart) {
+//     var cartItem=cart.length;
+//     var recentItem=cart[cart.length - 1];
+
+//     return [cartItem , recentItem]; /* অ্যারে হিসাবে  return করা হয়েছে। তাই ফাংশন কল করার  সময় console.log এর ভিতর কল করতে হয়   */
+// }
+
+// shoppingCart.push('Shavana');
+// console.log(cartDisplay(shoppingCart));
+// shoppingCart.push('kulsum');
+// console.log(cartDisplay(shoppingCart));
 
 
-// console.log(shoppingCart[3]);
+/* আইটেম ডিলিট করা */
+// shoppingCart.pop(); /* pop মেথড টা লাস্ট এর একটা আইটেম ডিলিট করে দেই  */
 
-// shoppingCart.push(null);
-// console.log(5 + null);
-
-
-// last iteam dekhar jonno function.
-
-
-shoppingCart.push('Shavana');
-newIteamAdd(shoppingCart);
-shoppingCart.push('kulsum');
-newIteamAdd(shoppingCart);
-shoppingCart.push('kabila');
-newIteamAdd(shoppingCart);
-
-function newIteamAdd (cart){
-    console.log(`There are ${shoppingCart.length} ittems in the shopping cart`);
-    console.log(`Recently added: ${shoppingCart[shoppingCart.length - 1]}`);
-}
-
-
-
-// Removing element form array. (last element delete)
-// shoppingCart.pop();
-// shoppingCart.pop();
 // console.log(shoppingCart);
 
-// specific element delete.
-var dletItem = shoppingCart.indexOf('Morjina');
-shoppingCart.slice(dletItem, 1);
-console.log(shoppingCart);
+/* নির্দিষ্ট কোন একটা আইটেম ডিলিট  */
+// var deleteItem = shoppingCart.indexOf('Jorina');  /* indexOf মেথড টা আইটেম এর ইনডেক্স নাম্বার আউটপুট দেই  */
+
+// shoppingCart.splice(deleteItem , 1); /* splice মেথড টা দিয়া নির্দিষ্ট আইটেম ডিলিট করা হয়। এখানে deleteItem অর্থাৎ আইটেম এর ইনডেক্স নাম্বার বুজাচ্ছে, পরের 1 দ্বারা বুঝাছে ডিলিট করা হবে। আর যদি 0 থাকতো , তাহলে ডিলিট করা হবে না বুঝাইত   */
+
+// console.log(shoppingCart);
